61cdebfcd2496f6e66e10d4ec075a123ef5e7427
BlackMAS <42680915+BlackMAS@users.noreply.github.com>
2018-11-12 23:18:54 -0500

    Add files via upload

---
+++ OddRatioAnalysis.R
@@ -0,0 +1,53 @@
+
+install.packages("reshape") 
+install.packages("gridExtra")
+
+#Loading libraries
+#data manipulation
+library(tidyverse)
+library(lubridate)
+library(reshape)# use transpose and melt
+library(dplyr)
+library(mice)# imputations
+
+#machine learning
+library(caret)
+library(gbm)
+library(randomForest)
+
+#graphics
+library(ggplot2)
+library(gridExtra)#plot multiple graphs on one plot
+library(corrplot)
+library(readxl)
+
+
+data1<-read_excel('Odds Ratios 11-12-18.xlsx', sheet = 1, range = cell_rows(2:26))
+
+data1$`[95% Conf. Interval]`<-str_replace_all(data1$`[95% Conf. Interval]`, "\\[|\\]", " ")
+
+data1$ORa<-str_replace_all(data1$ORa, "[*]", " ")
+
+
+data1 <- separate(data1, col = `[95% Conf. Interval]`, into = c('LowerLimit','UpperLimit'), sep = ",")
+
+cols3 <- c('ORa','LowerLimit','UpperLimit')
+data1[cols3] <- lapply(data1[cols3], as.numeric)
+
+
+
+p<- ggplot(data=data1, aes(x=Behavior,y=ORa))+
+  geom_point(aes(col=Behavior))+
+  geom_hline(aes(fill=Behavior), yintercept = 1, linetype=2)+
+  xlab('Variable')+ylab("aOR (95% Confidence Interval)")+
+  geom_errorbar(aes(ymin=LowerLimit, ymax=UpperLimit,col=Behavior),width=0.5, cex=1)+
+  facet_wrap(~Condition, strip.position = "left",nrow = 24, scales = "free_y")+
+  theme(plot.title = element_text(size = 16,face='bold'),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.text.x=element_text(face="bold"),
+        axis.title = element_text(size=12,face="bold"),
+        strip.text.y = element_text(hjust=0,vjust =1,angle=180,face="bold"))+
+  coord_flip()
+theme_
+p
